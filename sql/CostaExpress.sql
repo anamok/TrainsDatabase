f259c20be1fb7b39593bb6e9fe8ac53eb0662646
slavrinc <57003012+slavrinc@users.noreply.github.com>
2022-03-13 11:53:26 -0400

    Update CostaExpress.sql

--- sql/CostaExpress.sql
+++ sql/CostaExpress.sql
@@ -2,17 +2,16 @@ DROP TABLE IF EXISTS Station, Distance, Railline, Route, Schedule,
     Train, Reservation, Passenger, Clock, Ticket CASCADE;
 
 CREATE TABLE Station (
-    station_no INTEGER NOT NULL, -- keys should probably never be null
+    station_no INTEGER,
     hours VARCHAR(32),
     address VARCHAR(32),
 
-    CONSTRAINT Station_PK PRIMARY KEY(station_no),
-    CONSTRAINT Station_UK UNIQUE(station_no) -- not sure if we need this
+    CONSTRAINT Station_PK PRIMARY KEY(station_no)
 );
 
 CREATE TABLE Distance (
-    station_a INTEGER NOT NULL, -- use its station_no -- keys should probably never be null
-    station_b INTEGER NOT NULL, -- use its station_no -- keys should probably never be null
+    station_a INTEGER, -- use its station_no
+    station_b INTEGER, -- use its station_no
     miles INTEGER,
 
     CONSTRAINT Distance_PK PRIMARY KEY(station_a, station_b),
@@ -24,26 +23,24 @@ CREATE TABLE Distance (
 );
 
 CREATE TABLE Railline (
-    rail_id INTEGER NOT NULL, -- keys should probably never be null
+    rail_id INTEGER,
     station_a INTEGER,
     station_b INTEGER,
     speed_limit INTEGER,
 
     CONSTRAINT Railline_PK PRIMARY KEY(rail_id),
-    CONSTRAINT Railline_UN UNIQUE(rail_id), -- not sure if we need this
     CONSTRAINT Railline_FK_Distance FOREIGN KEY(station_a, station_b) REFERENCES Distance(station_a, station_b)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE Route (
-    route_id INTEGER NOT NULL, -- keys should probably never be null
+    route_id INTEGER,
     depart_station INTEGER,
     dest_station INTEGER,
     station INTEGER,
     open BOOL,
 
     CONSTRAINT Route_PK PRIMARY KEY(route_id),
-    CONSTRAINT Route_UN UNIQUE(route_id), -- not sure if we need this
     CONSTRAINT Route_FK_DepartStation FOREIGN KEY(depart_station) REFERENCES Station(station_no)
         ON UPDATE CASCADE ON DELETE CASCADE,
     CONSTRAINT Route_FK_DestStation FOREIGN KEY(dest_station) REFERENCES Station(station_no)
@@ -53,13 +50,12 @@ CREATE TABLE Route (
 );
 
 CREATE TABLE Train ( -- moved train creation above schedule bc schedule now requires a train number
-    train_id INTEGER NOT NULL, -- keys should probably never be null
+    train_id INTEGER, 
     seats INTEGER,
     price_mi MONEY,
     top_speed INTEGER,
 
-    CONSTRAINT Train_PK PRIMARY KEY(train_id),
-    CONSTRAINT Train_UK UNIQUE(train_id) -- not sure if we need this
+    CONSTRAINT Train_PK PRIMARY KEY(train_id)
 );
 
 CREATE TABLE Schedule (
@@ -77,25 +73,23 @@ CREATE TABLE Schedule (
 );
 
 CREATE TABLE Reservation (
-    reserv_no INTEGER NOT NULL, -- keys should probably never be null
+    reserv_no INTEGER,
     price MONEY,
     payments MONEY,
     balance MONEY,
 
-    CONSTRAINT Reservation_PK PRIMARY KEY(reserv_no),
-    CONSTRAINT Reservation_UN UNIQUE(reserv_no) -- not sure if we need this
+    CONSTRAINT Reservation_PK PRIMARY KEY(reserv_no)
 );
 
 CREATE TABLE Passenger (
-    customer_id INTEGER NOT NULL, -- keys should probably never be null
+    customer_id INTEGER,
     first_name VARCHAR(32),
     last_name VARCHAR(32),
     email VARCHAR(32),
     phone_no VARCHAR(32),
     address VARCHAR(32),
 
-    CONSTRAINT Passenger_PK PRIMARY KEY(customer_id),
-    CONSTRAINT Passenger_UK UNIQUE(customer_id) -- not sure if we need this
+    CONSTRAINT Passenger_PK PRIMARY KEY(customer_id)
 );
 
 CREATE TABLE Clock (
@@ -105,11 +99,10 @@ CREATE TABLE Clock (
 );
 
 CREATE TABLE Ticket (
-    ticket_no INTEGER NOT NULL, -- keys should probably never be null
+    ticket_no INTEGER,
     reserv_no INTEGER,
 
     CONSTRAINT Ticket_PK PRIMARY KEY(ticket_no),
-    CONSTRAINT Ticket_UN UNIQUE(ticket_no), -- not sure if we need this
     CONSTRAINT Ticket_FK_Reserv FOREIGN KEY(reserv_no) REFERENCES Reservation(reserv_no)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
